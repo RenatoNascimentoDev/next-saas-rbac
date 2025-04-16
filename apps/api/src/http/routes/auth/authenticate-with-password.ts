1194fe66e5d58d5caabfbf13426c5246a3c9a4db
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-04-16 19:03:41 +0000

    Rota: Autenticação via senha

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/lib/prisma'
+import { compare } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with e-mail & password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string(),
+        }),
+      },
+    },
+    async (request, replay) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        return replay.status(400).send({ message: 'Invalid credentials.' })
+      }
+
+      if (userFromEmail.passwordHash == null) {
+        return replay
+          .status(400)
+          .send({ message: 'User does not have a password, use social login.' })
+      }
+
+      const isPasswordValid = await compare(
+        password,
+        userFromEmail.passwordHash
+      )
+
+      if (!isPasswordValid) {
+        return replay.status(400).send({ message: 'Invalid credentials.' })
+      }
+
+      const token = await replay.jwtSign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return replay.status(201).send({ token })
+    }
+  )
+}
