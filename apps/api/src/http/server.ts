d0035bacc8d73ff835023b3ec79c40fdf42ad888
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-04-23 00:32:14 +0000

    Rota: Criação de organização

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,7 @@ import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { requestPassword } from './routes/auth/reset-password'
 import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { env } from '@saas/env'
+import { createOrganization } from './routes/orgs/create-organization'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -63,6 +64,8 @@ app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(requestPassword)
+
+app.register(createOrganization)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🚀HTTP server running!')
