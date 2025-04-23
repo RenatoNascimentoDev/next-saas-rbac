c552385e8fb028d8709efc828bc757346fcc9bdd
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-04-23 18:44:30 +0000

    Rota: Criação de projetos

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { getOrganization } from './routes/orgs/get-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { shutdownOrganization } from './routes/orgs/shutdown-organizations'
 import { transferOrganization } from './routes/orgs/transfer-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -78,6 +79,7 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('🚀HTTP server running!')
