91d40fb37dc43d4b8fb42d0909eab442d1d253c1
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-04-29 23:18:35 +0000

    Evitando reset do formulário

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -1,27 +1,48 @@
 'use client'
 
 import { AlertTriangle, Loader2 } from 'lucide-react'
-
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState, useTransition, type FormEvent } from 'react'
 
 import githubIcon from '@/assets/github-icon.svg'
-import { Label } from '@/components/ui/label'
-import { signInWithEmailAndPassword } from './actions'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Separator } from '@/components/ui/separator'
-import { useActionState } from 'react'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+
+import { signInWithEmailAndPassword } from './actions'
 
 export function SignInForm() {
-  const [{ success, message, errors }, formAction, isPending] = useActionState(
-    signInWithEmailAndPassword,
-    { success: false, message: null, errors: null }
-  )
+  const [isPending, startTransition] = useTransition()
+
+  const [{ success, message, errors }, setFormState] = useState<{
+    success: boolean
+    message: string | null
+    errors: Record<string, string[]> | null
+  }>({
+    success: false,
+    message: null,
+    errors: null,
+  })
+
+  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const form = event.currentTarget
+
+    const data = new FormData(form)
+
+    startTransition(async () => {
+      const state = await signInWithEmailAndPassword(data)
+
+      setFormState(state)
+    })
+  }
 
   return (
-    <form action={formAction} className="space-y-4">
+    <form onSubmit={handleSignIn} className="space-y-4">
       {success == false && message && (
         <Alert variant="destructive">
           <AlertTriangle className="size-4" />
