cdcb743449cc9846e479bc8277582afceae98ba6
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-04-29 22:36:30 +0000

    Estados de erro do formulário

--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -1,23 +1,51 @@
 'use server'
 
+import { z } from 'zod'
+
 import { signInWithPassword } from '@/http/sign-in-with-password'
+import { HTTPError } from 'ky'
 
-export async function signInWithEmailAndPassword(
-  previousState: unknown,
-  data: FormData
-) {
-  console.log(previousState)
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide a password.' }),
+})
 
-  const { email, password } = Object.fromEntries(data)
+export async function signInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signInSchema.safeParse(Object.fromEntries(data))
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
 
   await new Promise((resolve) => setTimeout(resolve, 2000))
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
 
-  console.log(result)
+    console.log(token)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
 
-  return 'Success'
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
