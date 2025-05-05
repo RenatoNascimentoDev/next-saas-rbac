81a8238facbac480d985275c3daf1f2869ce62cf
Renato Nascimento <renatocorreianascimento@gmail.com>
2025-05-05 11:32:31 +0000

    ajustando o client id

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -5,10 +5,10 @@ import { redirect } from 'next/navigation'
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInURL.searchParams.set('client_id', 'Ov23liTyQqR9lxGE62bG')
+  githubSignInURL.searchParams.set('client_id', 'Ov23liCvSeXJrWHTAC0d')
   githubSignInURL.searchParams.set(
     'redirect_uri',
-    'https://next-saas-rbac-web-vt3e.vercel.app/api/auth/callback',
+    'https://next-saas-rbac-web-vt3e.vercel.app/api/auth/callback'
   )
   githubSignInURL.searchParams.set('scope', 'user')
 
