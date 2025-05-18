2bf24a18738cb7a581fa95c5aef1abf16cf74f4f
Renato Nascimento <renatocorreianascimento@gmail.com>
2025-05-18 21:47:11 +0000

    Action: remover membro

--- apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -7,9 +7,10 @@ import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { getMembers } from '@/http/get-members'
 import { getMembership } from '@/http/get-membership'
 import { getOrganization } from '@/http/get-organization'
-import { ArrowLeftRight, Crown } from 'lucide-react'
+import { ArrowLeftRight, Crown, UserMinus } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { organizationSchema } from '@saas/auth'
+import { removeMemberAction } from './actions'
 
 export async function MemberList() {
   const currentOrg = await getCurrentOrg()
@@ -75,6 +76,23 @@ export async function MemberList() {
                           <ArrowLeftRight className="mr-2 size-2" />
                           Transfer ownership
                         </Button>
+                      )}
+
+                      {permissions?.can('delete', 'User') && (
+                        <form action={removeMemberAction.bind(null, member.id)}>
+                          <Button
+                            disabled={
+                              member.userId === membership.userId ||
+                              member.userId === organization.ownerId
+                            }
+                            type="submit"
+                            size="sm"
+                            variant="destructive"
+                          >
+                            <UserMinus className="mr-2 size-4" />
+                            Remove
+                          </Button>
+                        </form>
                       )}
                     </div>
                   </TableCell>
